ab0f200193c7b1b75945b6a31afc238cac86c650
Ayesha Siddiqua <siddiquaayesha18.2003@gmail.com>
2023-12-07 19:38:32 +0530

    added store and auth

--- Blog_Project/src/App.jsx
+++ Blog_Project/src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading]=useState(true)
+  const dispatch=useDispatch()
 
   return (
     <>
